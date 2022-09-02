2a9082fb87b29fdb168f26f2282eb7193a530ef9
Jim Dowling <jim@logicalclocks.com>
2022-09-02 16:57:22 +0200

    add streamlit example

---
+++ nbs/01-module/streamlit-example.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+
+st.title("Streamlit for ServerlessML")
+st.header("Easy UI in Python with Streamlit")
+
+chart_data = pd.DataFrame(np.random.randn(30, 3),
+columns=["Data Engineers", "Data Scientists", "ML Engineers"])
+
+st.bar_chart(chart_data)
+
