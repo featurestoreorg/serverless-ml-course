cf3782f6759ed32b4e90804faa1968f14a77a8e3
Jim Dowling <jim@logicalclocks.com>
2022-09-13 17:49:24 +0200

    batch inference

--- src/01-module/scripts/run-feature-and-prediction-pipelines.sh
+++ src/01-module/scripts/run-feature-and-prediction-pipelines.sh
@@ -5,7 +5,7 @@ set -e
 cd src/01-module
 
 echo "Running synthetic credit card transactions feature pipeline"
-jupyter nbconvert --to notebook --execute 2_cc_feature_pipeline.ipynb
+jupyter nbconvert --to notebook --execute feature_pipeline.ipynb
 echo "Running batch prediction pipeline"
 jupyter nbconvert --to notebook --execute 5_batch_predictions.ipynb
 
