62017e766d1b8fb771d616884acdafe488acb977
Jim Dowling <jim@logicalclocks.com>
2022-09-13 23:07:08 +0200

    updates

--- src/00-intro/streamlit-example.py
+++ src/00-intro/streamlit-example.py
@@ -1,3 +1,4 @@
+# +
 import pandas as pd
 import streamlit as st
 import numpy as np
@@ -9,4 +10,3 @@ chart_data = pd.DataFrame(np.random.randn(30, 3),
 columns=["Data Engineers", "Data Scientists", "ML Engineers"])
 
 st.bar_chart(chart_data)
-
--- src/01-module/scripts/run-feature-and-prediction-pipelines.sh
+++ src/01-module/scripts/run-feature-and-prediction-pipelines.sh
@@ -4,8 +4,7 @@ set -e
 
 cd src/01-module
 
-echo "Running synthetic credit card transactions feature pipeline"
-jupyter nbconvert --to notebook --execute feature_pipeline.ipynb
-echo "Running batch prediction pipeline"
-jupyter nbconvert --to notebook --execute 5_batch_predictions.ipynb
+jupyter nbconvert --to notebook --execute iris-feature-pipeline.ipynb
+jupyter nbconvert --to notebook --execute iris_batch_inference_pipeline.ipynb
+
 
