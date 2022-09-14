c44821b9169e550d322031f102a2ae2de7c30dfd
Jim Dowling <jim@logicalclocks.com>
2022-09-14 07:20:32 +0200

    add label for prediction

--- src/01-module/scripts/run-feature-and-prediction-pipelines.sh
+++ src/01-module/scripts/run-feature-and-prediction-pipelines.sh
@@ -5,6 +5,6 @@ set -e
 cd src/01-module
 
 jupyter nbconvert --to notebook --execute iris-feature-pipeline.ipynb
-jupyter nbconvert --to notebook --execute iris_batch_inference_pipeline.ipynb
+jupyter nbconvert --to notebook --execute iris-batch-inference-pipeline.ipynb
 
 
